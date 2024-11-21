474671f3455fa2eb29156b325f78cd49ade35f25
Kira1108 <327485253@163.com>
2024-11-21 15:06:56 +0800

    Initial commit

--- model.py
+++ model.py
@@ -7,18 +7,21 @@ from typing import List
 import torch
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
-@lru_cache(maxsize = None)
+
+@lru_cache(maxsize=None)
 def load_huggingface_model(
-    model_name_or_path:str = 'BAAI/bge-reranker-base', 
-    device:str = 'cuda'):
-    
-    logging.info(f"Loading huggingface model {model_name_or_path}")       
+        model_name_or_path: str = 'BAAI/bge-reranker-base',
+        device: str = 'cuda'):
+
+    logging.info(f"Loading huggingface model {model_name_or_path}")
     tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
-    model = AutoModelForSequenceClassification.from_pretrained(model_name_or_path)
+    model = AutoModelForSequenceClassification.from_pretrained(
+        model_name_or_path)
     model.eval()
     model.to(device)
     logging.info(f"Model {model_name_or_path} loaded.")
     return model, tokenizer
+
 
 @dataclass
 class Reranker:
@@ -27,29 +30,29 @@ class Reranker:
     Conclusion: If there is already a deployed model, do not change it in any case.
     """
 
-    model_name_or_path:str = 'BAAI/bge-reranker-base'
+    model_name_or_path: str = 'BAAI/bge-reranker-base'
 
     def __post_init__(self):
         self.device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
         self.model, self.tokenizer = load_huggingface_model(
-            model_name_or_path = self.model_name_or_path, 
-            device = self.device)
+            model_name_or_path=self.model_name_or_path,
+            device=self.device)
 
-    def rerank(self, query:str, documents:List[str]) -> List[float]:
-        import torch
+    def rerank(self, query: str, documents: List[str]) -> List[float]:
         print(f"Reranking {len(documents)} documents...")
         start = time.time()
         pairs = [(str(query), str(doc)) for doc in documents]
-        
+
         with torch.no_grad():
             inputs = self.tokenizer(
-                pairs, padding=True, 
-                truncation=True, 
-                return_tensors='pt', 
+                pairs, padding=True,
+                truncation=True,
+                return_tensors='pt',
                 max_length=512)
-            
+
             inputs.to(self.device)
-            scores = self.model(**inputs, return_dict=True).logits.view(-1, ).float()
+            scores = self.model(
+                **inputs, return_dict=True).logits.view(-1, ).float()
             scores = scores.cpu().numpy().astype('float')
         end = time.time()
         print(f"Reranking taking {end-start:.2f} seconds")
